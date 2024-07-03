10670486aad1d0a37ce8e6fc7848b4cdbcbdc72d
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-07-04 03:47:43 +0530

    add loops Basics

---
+++ Basic_5/loop.js
@@ -0,0 +1,34 @@
+// For Loop
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(`2 x ${index} = ${2 * index}`);
+// }
+
+// break and continue
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(`${index}`);
+//     // if (index == 5) {
+//     //     console.log(`${index} detected `);
+//     //     break;
+//     // }
+//     if (index == 5) {
+//         continue;
+//     }
+//     console.log("hello");
+// }
+
+// While Loop
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(`${i}`);
+//     i++;
+// }
+
+
+// do while loop
+// score = 11
+// do {
+//     console.log(score);
+// } while (score < 10);
