8a5e84cecaf5166736832ee6c9555b5a633954c1
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-07-14 20:37:12 +0530

    Day - 1 Challenge

---
+++ 30-Day-Challenge/Day-1/Day-1-VariableAndDataType.js
@@ -0,0 +1,43 @@
+// Task-1
+var num;
+num = 10;
+console.log(num);
+
+// Task - 2
+let str;
+str = "Task - 2";
+console.log(str);
+
+// Task - 3
+// * Constant must be initialized at declaration
+const bool = true;
+console.log(bool);
+
+// Task - 4
+let num_4 = 100;
+let str_4 = "String-4";
+let bool_4 = false;
+const OBJ_4 = {
+    id: 14214,
+    name: "Kamlesh Sigh",
+    address: "Haldwani"
+}
+const arr_4 = [10, 20, 30, 40, 50];
+
+console.log(`Number : ${typeof num_4}`);
+console.log(`String : ${typeof str_4}`);
+console.log(`Boolean : ${typeof bool_4}`);
+console.log(`Object : ${typeof OBJ_4}`);// object
+console.log(`Array : ${typeof arr_4}`);// object
+
+// Task - 5
+let name = "Kamlesh"
+console.log(name);
+name = "Kamlesh Singh";
+console.log(name);
+
+// Task - 6
+const PI = 3.14;
+console.log(PI);
+// PI = 3.14159; // Give Error - can not assign value to constant variable
+console.log(PI);
