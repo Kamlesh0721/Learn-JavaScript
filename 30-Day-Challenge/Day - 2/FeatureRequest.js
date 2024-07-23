374ca7f7018deb77449fa4945b113d63b253d7eb
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-07-24 03:47:23 +0530

    Complete upto day - 3

---
+++ 30-Day-Challenge/Day - 2/FeatureRequest.js
@@ -0,0 +1,3 @@
+// ternay operator
+let num = 10;
+(num >= 0) ? console.log("Positive") : console.log("Negative");
---
+++ 30-Day-Challenge/Day - 2/Operator.js
@@ -0,0 +1,23 @@
+// Activity - 3
+
+let x = 5;
+// console.log(x == 5);        // true
+// console.log(x == '5');      // true  (equal value only)
+// console.log(x === '5');     // false (equal value and equal type)
+
+// console.log(x != '5');
+// console.log(x !== '5');
+
+// console.log(x >= '5');
+
+// Activity - 4
+// const gender = 'Male';
+// let age = 16;
+// console.log(age >= 18 && gender === 'Male');
+// console.log(age >= 18 || gender === 'Male');
+// console.log(age != 18);
+
+// Activity - 5
+let currAge = 18;
+let votable = (currAge > 18) ? "Eligible" : "Not Eligible";
+console.log(votable);
