a2c8ac9ac31686c11efd6ef1e7344985781d1810
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-07-24 03:46:02 +0530

    complete upto day - 3

---
+++ 30-Day-Challenge/Day - 3/leapYear.js
@@ -0,0 +1,16 @@
+let year = 1986;
+if (year % 4 == 0) {
+    if (year % 100 == 0) {
+        if (year % 400 == 0) {
+            console.log(`${year} is a leap year`);
+        }
+        else {
+            console.log(`${year} is not a leap year`);
+        }
+    }
+    else {
+        console.log(`${year} is a leap year`);
+    }
+} else {
+    console.log(`${year} is not a leap year`);
+}
