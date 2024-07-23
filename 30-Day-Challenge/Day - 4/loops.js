e2a50942785773da715a29c791751bac5dcade54
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-07-24 04:03:04 +0530

    add day - 4

---
+++ 30-Day-Challenge/Day - 4/loops.js
@@ -0,0 +1,14 @@
+// Activity - 2
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+// Activity - 3
+let num = 5;
+let ans = 1;
+do {
+    ans = ans * num;
+    num--;
+} while (num != 1);
+
+console.log(ans);
