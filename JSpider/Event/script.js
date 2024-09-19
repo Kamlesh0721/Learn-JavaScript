fa6e357dfed1f0fa2cdfe87b824480c62c4e3140
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-09-19 15:19:56 +0530

    dark mode using button

--- JSpider/Event/script.js
+++ JSpider/Event/script.js
@@ -1,4 +1,10 @@
 function switchMode() {
   const PAGE = document.querySelector(".page");
   PAGE.classList.toggle("dark");
+
+  const DAY_BUTTON=document.querySelector(".fa-sun");
+  const NIGHT_BUTTON=document.querySelector(".fa-moon");
+
+  DAY_BUTTON.classList.toggle("hide");
+  NIGHT_BUTTON.classList.toggle("hide");
 }
